f7d8c26c8acda497ab7b829880f6deb929bf4775
johanback <johan.g.back@gmail.com>
2020-06-01 17:23:01 +0200

    Lists are now fetched from backend. Working on adding new lists.

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,37 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import SidebarListButton from './SidebarListButton';
+import { Button } from 'react-bootstrap';
 
 function Sidebar(props) {
 
-  const [lists, setLists] = useState(
-    [
-      {
-        "id": 1,
-        "listName": "Groceries"
+  const [lists, setLists] = useState([]);
+  useEffect(() => { getAllLists() })
+
+  // const listsPlaceholder = [
+  //   {
+  //     "id": 1,
+  //     "listName": "Groceries"
+  //   },
+  //   {
+  //     "id": 2,
+  //     "listName": "Workout"
+  //   },
+  //   {
+  //     "id": 3,
+  //     "listName": "Work"
+  //   },
+  //   {
+  //     "id": 4,
+  //     "listName": "Hobbies"
+  //   }
+  // ];
+
+  function getAllLists() {
+    fetch("http://localhost:8080/lists/")
+        .then(result => result.json())
+        .then(result => {
+          setLists(result)
+        })
+  }
+
+  async function addNewList(url = 'http://localhost:8080/lists',
+    data = { 'newList' : 'The New List' }) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      {
-        "id": 2,
-        "listName": "Workout"
-      },
-      {
-        "id": 3,
-        "listName": "Work"
-      },
-      {
-        "id": 4,
-        "listName": "Hobbies"
-      }
-    ]
-  )
+      body: JSON.stringify(data) 
+    });
+    return response.json(); 
+  }
 
   return (
     <React.Fragment>
       <div className="sidebartitle">
         To Do's
       </div>
-      <div>
+      <div className="toDoLists">
         {lists.map(x => <SidebarListButton id={x.id} listName={x.listName} setActiveList={props.setActiveList}/>)}
+      </div>
+      <div className="newListButton">
+        <Button variant="outline-secondary" onClick={addNewList}>New List</Button>
       </div>
     </React.Fragment>
   )
--- src/Components/ToDoView/ToDoView.js
+++ src/Components/ToDoView/ToDoView.js
@@ -5,62 +5,6 @@ import { ListGroup } from 'react-bootstrap';
 
 function ToDoView(props) {
 
-    const allToDos = [
-        {
-            "id": 1,
-            "parent": {
-                "id": 1,
-                "listName": "Groceries"
-            },
-            "toDoDescription": "Apples",
-            "completed": false
-        },
-        {
-            "id": 2,
-            "parent": {
-                "id": 1,
-                "listName": "Groceries"
-            },
-            "toDoDescription": "Pasta",
-            "completed": false
-        },
-        {
-            "id": 3,
-            "parent": {
-                "id": 1,
-                "listName": "Groceries"
-            },
-            "toDoDescription": "Coffee",
-            "completed": false
-        },
-        {
-            "id": 4,
-            "parent": {
-                "id": 2,
-                "listName": "Workout"
-            },
-            "toDoDescription": "Deadlift",
-            "completed": false
-        },
-        {
-            "id": 5,
-            "parent": {
-                "id": 2,
-                "listName": "Workout"
-            },
-            "toDoDescription": "Squat",
-            "completed": false
-        },
-        {
-            "id": 6,
-            "parent": {
-                "id": 2,
-                "listName": "Workout"
-            },
-            "toDoDescription": "Bench press",
-            "completed": false
-        }
-    ]
     const [currentToDo, setCurrentToDo] = useState([]);
     const [currentToDoTitle, setCurrentToDoTitle] = useState("");
     const [currentToDoLoaded, setCurrentToDoLoaded] = useState(false);
